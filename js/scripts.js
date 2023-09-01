a4a88f48d45d0e993b39ddca8679230f65612790
Isaac Barcroft <i_barcroft@yahoo.com>
2023-09-01 15:02:08 -0400

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const button = document.querySelector('button');
+const items = document.querySelectorAll('.input');
+const display = document.getElementById('myList');
+const ul = document.createElement('ol');
+
+display.appendChild(ul)
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    ul.innerHTML = "";
+    items.forEach(item => {
+        if (item.value) {
+            ul.appendChild(document.createElement('li')).textContent = item.value;
+        }
+    })
+})
